e9fd1ce95c41c32793bb6bcd00cfd424f9cdeb43
vignesh <vignesh.bharani@1mg.com>
2023-03-13 15:16:32 +0530

    Products changes

---
+++ src/main/java/com/StockManagement/model/Products.java
@@ -0,0 +1,80 @@
+package com.StockManagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "products")
+public class Products {
+
+    @Id   
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String category;
+    private String description;
+    private float selling_price;
+    private int in_stock;
+    private boolean active;
+
+    public Products() {
+    }
+
+    public Products(String name, String category, String description, float selling_price, int in_stock,
+            boolean active) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.selling_price = selling_price;
+        this.in_stock = in_stock;
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public float getSelling_price() {
+        return selling_price;
+    }
+    public void setSelling_price(float selling_price) {
+        this.selling_price = selling_price;
+    }
+    public int getIn_stock() {
+        return in_stock;
+    }
+    public void setIn_stock(int in_stock) {
+        this.in_stock = in_stock;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+}
